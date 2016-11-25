2c459ee3ea810576d48102176bad0dc9132f8df1
Pranav Dixit <pranavdixit20@gmail.com>
2016-11-25 19:15:46 +0530

    80% done

--- app/src/main/java/adservices/atmmadat/MainActivity.java
+++ app/src/main/java/adservices/atmmadat/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.Locale;
@@ -19,11 +22,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Locale myLocale;
     Bundle b;
     Intent in;
-    String lang = "en";
+    String lang = "en",uid="ca-app-pub-3715652664770345/9286598713";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(getApplicationContext(),uid);
+
+        AdView adView = new AdView(this);
+        adView = (AdView)findViewById(R.id.ad);
+        AdRequest ar=new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        adView.loadAd(ar);
+
         FirebaseStorage storage = FirebaseStorage.getInstance();
 
         eng = (Button)findViewById(R.id.eng);
@@ -128,7 +140,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 lang = "kn";
                 break;
             case R.id.nxt:
-               startActivity(new Intent(this,Welcome.class));
+               startActivity(new Intent(this,AboutAtmMec.class));
                 break;
             case R.id.exit:
                 moveTaskToBack(true);
