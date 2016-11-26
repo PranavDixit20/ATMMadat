2a0f234f188b37711968676cd0094170a3daa07d
Anupam Shembekar <anupam.s2006@gmail.com>
2016-11-26 15:52:25 -0800

    many changes

--- app/src/main/java/adservices/atmmadat/BasicInfo.java
+++ app/src/main/java/adservices/atmmadat/BasicInfo.java
@@ -8,17 +8,19 @@ import android.widget.Button;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class BasicInfo extends AppCompatActivity implements View.OnClickListener {
 
     String uid="ca-app-pub-3715652664770345/9286598713";
-    Button b,b1,b2,b3;
+    Button b,b1,b2,b3,b4,b5,b6,b7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_info);
 
+        MobileAds.initialize(getApplicationContext(),uid);
         AdView adView = new AdView(this);
         adView = (AdView)findViewById(R.id.ad);
         AdRequest ar=new AdRequest.Builder()
@@ -29,17 +31,37 @@ public class BasicInfo extends AppCompatActivity implements View.OnClickListener
         b = (Button)findViewById(R.id.bitoc);
         b1 = (Button)findViewById(R.id.biaci);
         b2 = (Button)findViewById(R.id.biback);
-        b3 = (Button)findViewById(R.id.binxt);
+        b3 = (Button)findViewById(R.id.syb);
+        b4 = (Button)findViewById(R.id.wod);
+        b5 =(Button)findViewById(R.id.emes);
+        b6 = (Button)findViewById(R.id.swc);
+        b7 = (Button)findViewById(R.id.so);
 
         b.setOnClickListener(this);
         b1.setOnClickListener(this);
         b2.setOnClickListener(this);
         b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
+            case R.id.wod:
+                startActivity(new Intent(this,WordDefination.class));
+                break;
+            case R.id.emes:
+                startActivity(new Intent(this,ErrorMessage.class));
+                break;
+            case R.id.swc:
+                startActivity(new Intent(this,ShopWithYourCard.class));
+                break;
+            case R.id.so:
+                startActivity(new Intent(this,ShopOnline.class));
+                break;
             case R.id.bitoc:
                 startActivity(new Intent(this,TypeOfCard.class));
                 break;
@@ -49,7 +71,7 @@ public class BasicInfo extends AppCompatActivity implements View.OnClickListener
             case R.id.biback:
                 startActivity(new Intent(this,MainActivity.class));
                 break;
-            case R.id.binxt:
+            case R.id.syb:
                 startActivity(new Intent(this,Welcome.class));
                 break;
 
--- app/src/main/java/adservices/atmmadat/TypeOfCard.java
+++ app/src/main/java/adservices/atmmadat/TypeOfCard.java
@@ -7,31 +7,57 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class TypeOfCard extends AppCompatActivity implements View.OnClickListener {
 
-    TextView tv,tv1,tv2;
-    Button b;
+    Button b,b1,b2,b3,b4;
+    String uid = "ca-app-pub-3715652664770345/9286598713";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_type_of_card);
 
-        tv = (TextView)findViewById(R.id.vcard);
-        tv1 = (TextView)findViewById(R.id.rcard);
-        tv2 = (TextView)findViewById(R.id.mcard);
-        b = (Button)findViewById(R.id.tocnxt);
+        MobileAds.initialize(getApplicationContext(),uid);
+
+        AdView adView = new AdView(this);
+        adView = (AdView)findViewById(R.id.ad);
+        AdRequest ar=new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        adView.loadAd(ar);
+
+        b = (Button) findViewById(R.id.vcard);
+        b1 = (Button) findViewById(R.id.rcard);
+        b2 = (Button) findViewById(R.id.mcard);
+        b3 = (Button)findViewById(R.id.pcard);
+        b4 = (Button)findViewById(R.id.tocnxt);
 
         b.setOnClickListener(this);
-        tv.setText(R.string.vcard);
-        tv1.setText(R.string.rcard);
-        tv2.setText(R.string.mcard);
-
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
+            case R.id.vcard:
+                startActivity(new Intent(this,CreditCard.class));
+                break;
+            case R.id.rcard:
+                startActivity(new Intent(this,AtmCard.class));
+                break;
+            case R.id.mcard:
+                startActivity(new Intent(this,DebitATMCard.class));
+                break;
+            case R.id.pcard:
+                startActivity(new Intent(this,PaymentGetway.class));
+                break;
             case R.id.tocnxt:
                 startActivity(new Intent(this,BasicInfo.class));
                 break;
